b01a3dbe59eeba63dc99545f499899ab54efd769
John Kouris <jdkouris@gmail.com>
2020-01-14 19:50:54 -0800

    Implemented filter method and connected the sliders

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -25,7 +25,18 @@ class PhotoFilterViewController: UIViewController {
     }
     
     private func image(byFiltering image: UIImage) -> UIImage {
-        return image
+        guard let cgImage = image.cgImage else { return image }
+        let inputImage = CIImage(cgImage: cgImage)
+        
+        filter.inputImage = inputImage
+        filter.saturation = saturationSlider.value
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        
+        guard let outputImage = filter.outputImage else { return image }
+        guard let renderedImage = context.createCGImage(outputImage, from: outputImage.extent) else { return image }
+        
+        return UIImage(cgImage: renderedImage)
     }
     
     private func updateImage() {
@@ -63,15 +74,15 @@ class PhotoFilterViewController: UIViewController {
     // MARK: Slider events
     
     @IBAction func brightnessChanged(_ sender: UISlider) {
-
+        updateImage()
     }
     
     @IBAction func contrastChanged(_ sender: Any) {
-
+        updateImage()
     }
     
     @IBAction func saturationChanged(_ sender: Any) {
-
+        updateImage()
     }
 }
 

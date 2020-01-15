331e8988fff74ef2b25f6fa70b6dc841b715c2d6
John Kouris <jdkouris@gmail.com>
2020-01-14 19:14:18 -0800

    Set up the imagePickerController

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -4,40 +4,75 @@ import Photos
 
 class PhotoFilterViewController: UIViewController {
 
-	@IBOutlet weak var brightnessSlider: UISlider!
-	@IBOutlet weak var contrastSlider: UISlider!
-	@IBOutlet weak var saturationSlider: UISlider!
-	@IBOutlet weak var imageView: UIImageView!
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var originalImage: UIImage?
+    
+    var context = CIContext(options: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-	}
-	
-	// MARK: Actions
-	
-	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
-		// TODO: show the photo picker so we can choose on-device photos
-		// UIImagePickerController + Delegate
-	}
-	
-	@IBAction func savePhotoButtonPressed(_ sender: UIButton) {
-		// TODO: Save to photo library
-	}
-	
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func choosePhotoButtonPressed(_ sender: Any) {
+        // TODO: show the photo picker so we can choose on-device photos
+        // UIImagePickerController + Delegate
+        presentImagePickerController()
+    }
+    
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("The photo library is not available")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func savePhotoButtonPressed(_ sender: UIButton) {
+        // TODO: Save to photo library
+    }
+    
 
-	// MARK: Slider events
-	
-	@IBAction func brightnessChanged(_ sender: UISlider) {
+    // MARK: Slider events
+    
+    @IBAction func brightnessChanged(_ sender: UISlider) {
 
-	}
-	
-	@IBAction func contrastChanged(_ sender: Any) {
+    }
+    
+    @IBAction func contrastChanged(_ sender: Any) {
 
-	}
-	
-	@IBAction func saturationChanged(_ sender: Any) {
+    }
+    
+    @IBAction func saturationChanged(_ sender: Any) {
 
-	}
+    }
 }
 
+extension PhotoFilterViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
